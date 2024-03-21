d5907ee699057dda7d0d7c0ff053496e6bcb6951
Olimdzhon <write2sd@mail.ru>
2024-03-21 20:39:24 +0500

    Initial Commit

---
+++ HW1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HW1
+//
+//  Created by Олимджон Садыков on 21/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Олимджон Садыков on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
